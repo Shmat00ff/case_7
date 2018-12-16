eea44ee310d9353571e1842d980c8bbd08ef04cc
Shmat00ff <shmat00ff@gmail.com>
2018-12-16 20:56:33 +0700

    Commit

--- case.py
+++ case.py
@@ -4,3 +4,12 @@ Bayanova A., Shmatov D.
 
 """
 import random
+
+
+k = []
+s = list(map(str, input().split()))
+for i in range(len(s)):
+    if s[i][0] == s[i][0].upper():
+        k.append(s[i])
+
+print(k)
