fc0f2fc6b7f3227645c801d4888ef0edfcf89b34
Shmat00ff <shmat00ff@gmail.com>
2018-12-16 21:47:41 +0700

    Commit

--- case.py
+++ case.py
@@ -4,42 +4,44 @@ Bayanova A., Shmatov D.
 
 """
 import random
+
 first = []
 last = []
 
 n = input()
 
-def sp(n):
-    n = n.replace("&", "")
-    n = n.replace("^", "")
-    n = n.replace("%", "")
-    n = n.replace("$", "")
-    n = n.replace("#", "")
-    n = n.replace("*", "")
-    n = n.replace("-", "")
-    n = n.replace("=", "")
-    n = n.replace("+", "")
-    n = n.replace("@", "")
-    n = n.replace(")", "")
-    n = n.replace("(", "")
-    n = n.replace("/", "")
-    n = n.replace(":", "")
-    n = n.replace(";", "")
+n = n.replace("\n", " ")
+n = n.replace("&", "")
+n = n.replace("^", "")
+n = n.replace("%", "")
+n = n.replace("$", "")
+n = n.replace("#", "")
+n = n.replace("*", "")
+n = n.replace("-", "")
+n = n.replace("=", "")
+n = n.replace("+", "")
+n = n.replace("@", "")
+n = n.replace(")", "")
+n = n.replace("(", "")
+n = n.replace("/", "")
+n = n.replace(":", "")
+n = n.replace(";", "")
 
-    n = n.replace(" .", ".")
-    n = n.replace(" ,", ",")
-    n = n.replace(" !", "!")
-    n = n.replace(" ?", "?")
-    n = n.replace(".", ". ")
-    n = n.replace(":", ": ")
-    n = n.replace(",", ", ")
-    n = n.replace("!", "! ")
-    n = n.replace("?", "? ")
-    n = n.replace("  ", " ")
+n = n.replace(" .", ".")
+n = n.replace(" ,", ",")
+n = n.replace(" !", "!")
+n = n.replace(" ?", "?")
+n = n.replace(".", ". ")
+n = n.replace(",", ", ")
+n = n.replace("!", "! ")
+n = n.replace("?", "? ")
+n = n.replace("  ", " ")
 
-sp(n)
 
+d = {}
 s = list(map(str, n.split()))
+for j in range(1, len(s)):
+    d.update({s[j-1] : s[j]})
 
 for i in range(len(s)):
     if s[i][0] == s[i][0].upper():
@@ -49,6 +51,6 @@ for i in range(len(s)):
 for i in range(len(s)):
     if s[i][len(s[i])-1] == ".":
         last.append(s[i])
-
+print(d)
 print(last)
 print(first)
