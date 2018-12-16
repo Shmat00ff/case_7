c6973c925d8ac69dabe5948d934387d8dc11080f
Shmat00ff <shmat00ff@gmail.com>
2018-12-16 21:16:12 +0700

    Commit

--- case.py
+++ case.py
@@ -4,12 +4,51 @@ Bayanova A., Shmatov D.
 
 """
 import random
+first = []
+last = []
 
+n = input()
 
-k = []
-s = list(map(str, input().split()))
+def sp(n):
+    n = n.replace("&", "")
+    n = n.replace("^", "")
+    n = n.replace("%", "")
+    n = n.replace("$", "")
+    n = n.replace("#", "")
+    n = n.replace("*", "")
+    n = n.replace("-", "")
+    n = n.replace("=", "")
+    n = n.replace("+", "")
+    n = n.replace("@", "")
+    n = n.replace(")", "")
+    n = n.replace("(", "")
+    n = n.replace("/", "")
+    n = n.replace(":", "")
+    n = n.replace(";", "")
+
+    n = n.replace(" .", ".")
+    n = n.replace(" ,", ",")
+    n = n.replace(" !", "!")
+    n = n.replace(" ?", "?")
+    n = n.replace(".", ". ")
+    n = n.replace(":", ": ")
+    n = n.replace(",", ", ")
+    n = n.replace("!", "! ")
+    n = n.replace("?", "? ")
+    n = n.replace("  ", " ")
+
+sp(n)
+
+s = list(map(str, n.split()))
+
 for i in range(len(s)):
     if s[i][0] == s[i][0].upper():
-        k.append(s[i])
+        if not s[i][len(s[i])-1] == ".":
+            first.append(s[i])
 
-print(k)
+for i in range(len(s)):
+    if s[i][len(s[i])-1] == ".":
+        last.append(s[i])
+
+print(last)
+print(first)
