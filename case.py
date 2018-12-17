66b88134f5cf5346b51579aba4d55af590574e07
Shmat00ff <shmat00ff@gmail.com>
2018-12-17 18:38:49 +0700

    Commit

--- case.py
+++ case.py
@@ -39,18 +39,30 @@ n = n.replace("  ", " ")
 
 
 d = {}
+e = []
 s = list(map(str, n.split()))
 for j in range(1, len(s)):
-    d.update({s[j-1] : s[j]})
-
+    if s[j-1] in d:
+        m = "{} {}".format(d.get(s[j-1]), s[j])
+        d.update({s[j-1] : m})
+        if not s[j-1] in e:
+            e.append(s[j-1])
+    else:
+        d.update({s[j-1] : s[j]})
 for i in range(len(s)):
     if s[i][0] == s[i][0].upper():
         if not s[i][len(s[i])-1] == ".":
             first.append(s[i])
-
 for i in range(len(s)):
     if s[i][len(s[i])-1] == ".":
         last.append(s[i])
-print(d)
-print(last)
-print(first)
+
+print(first)  # выводит список первых слов
+
+print(last)  # выводит список последних слов
+
+print(e)  # выводит список слов не с одним значением ключа
+
+print(d)  # выводит словарь
+
+
