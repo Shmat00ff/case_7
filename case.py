3e7aff25f408bf58d232b2e8479937eeb40a38db
Shmat00ff <Shmat00ff@gmail.com>
2018-12-18 10:24:38 +0700

    Commit

--- case.py
+++ case.py
@@ -38,17 +38,17 @@ n = n.replace("?", "? ")
 n = n.replace("  ", " ")
 
 
-d = {}
-e = []
+dic = {}
+keys = []
 s = list(map(str, n.split()))
 for j in range(1, len(s)):
 
-    if not s[j-1] in d:
-        d[s[j-1]] = []
-    d[s[j - 1]].append(s[j])
-    if  len(d[s[j - 1]]) > 1:
-        if s[j-1] not in e:
-            e.append(s[j - 1])
+    if not s[j-1] in dic:
+        dic[s[j-1]] = []
+    dic[s[j - 1]].append(s[j])
+    if len(dic[s[j - 1]]) > 1:
+        if s[j-1] not in keys:
+            keys.append(s[j - 1])
 
 for i in range(len(s)):
     if s[i][0] == s[i][0].upper():
@@ -62,8 +62,8 @@ print(first)  # выводит список первых слов
 
 print(last)  # выводит список последних слов
 
-print(e)  # выводит список слов не с одним значением ключа
+print(keys)  # выводит список слов не с одним значением ключа
 
-print(d)  # выводит словарь
+print(dic)  # выводит словарь
 
 
