97b4cba445ff5dcc1fbe9e97d8151df7f16b8cb9
Shmat00ff <Shmat00ff@gmail.com>
2018-12-18 10:21:24 +0700

    Commit

--- case.py
+++ case.py
@@ -42,20 +42,21 @@ d = {}
 e = []
 s = list(map(str, n.split()))
 for j in range(1, len(s)):
-    if s[j-1] in d:
-        m = "{} {}".format(d.get(s[j-1]), s[j])
-        d.update({s[j-1] : m})
-        if not s[j-1] in e:
-            e.append(s[j-1])
-    else:
-        d.update({s[j-1] : s[j]})
+
+    if not s[j-1] in d:
+        d[s[j-1]] = []
+    d[s[j - 1]].append(s[j])
+    if  len(d[s[j - 1]]) > 1:
+        if s[j-1] not in e:
+            e.append(s[j - 1])
+
 for i in range(len(s)):
     if s[i][0] == s[i][0].upper():
-        if not s[i][len(s[i])-1] == ".":
+        if s[i][-1] not in "!?.":
             first.append(s[i])
-for i in range(len(s)):
-    if s[i][len(s[i])-1] == ".":
+    if s[i][-1] in ".!?":
         last.append(s[i])
+
 
 print(first)  # выводит список первых слов
 
