d67192b5c8978a128f21de7a7f5b80ef14eac23b
AnyaBayanova <Bayanova-anna@mail.ru>
2018-12-21 10:08:50 +0700

    commiteee

--- case.py
+++ case.py
@@ -8,71 +8,78 @@ import random
 first = []
 last = []
 
-n = input('Введите имя файла:')
-pov = int(input('Количество генерируемых предложений: '))
+text = input('Введите имя файла: ')
+with open(text) as n:
 
-n = n.replace("\n", " ")
-n = n.replace("&", "")
-n = n.replace("^", "")
-n = n.replace("%", "")
-n = n.replace("$", "")
-n = n.replace("#", "")
-n = n.replace("*", "")
-n = n.replace("-", "")
-n = n.replace("=", "")
-n = n.replace("+", "")
-n = n.replace("@", "")
-n = n.replace(")", "")
-n = n.replace("(", "")
-n = n.replace("/", "")
-n = n.replace(":", "")
-n = n.replace(";", "")
+    pov = int(input('Количество генерируемых предложений: '))
+    for line in n:
+        n = line.replace("\n", " ")
+        n = line.replace("&", "")
+        n = line.replace("^", "")
+        n = line.replace("%", "")
+        n = line.replace("$", "")
+        n = line.replace("#", "")
+        n = line.replace("*", "")
+        n = line.replace("-", "")
+        n = line.replace("=", "")
+        n = line.replace("+", "")
+        n = line.replace("@", "")
+        n = line.replace(")", "")
+        n = line.replace("(", "")
+        n = line.replace("/", "")
+        n = line.replace(":", "")
+        n = line.replace(";", "")
 
-n = n.replace(" .", ".")
-n = n.replace(" ,", ",")
-n = n.replace(" !", "!")
-n = n.replace(" ?", "?")
-n = n.replace(".", ". ")
-n = n.replace(",", ", ")
-n = n.replace("!", "! ")
-n = n.replace("?", "? ")
-n = n.replace("  ", " ")
-
-
-d = {}
-e = []
-s = list(map(str, n.split()))
-for j in range(1, len(s)):
-
-    if not s[j-1] in d:
-        d[s[j-1]] = []
-    d[s[j - 1]].append(s[j])
-    if  len(d[s[j - 1]]) > 1:
-        if s[j-1] not in e:
-            e.append(s[j - 1])
-
-for i in range(len(s)):
-    if s[i][0] == s[i][0].upper():
-        if s[i][-1] not in "!?.":
-            first.append(s[i])
-    if s[i][-1] in ".!?":
-        last.append(s[i])
+        n = line.replace(" .", ".")
+        n = line.replace(" ,", ",")
+        n = line.replace(" !", "!")
+        n = line.replace(" ?", "?")
+        n = line.replace(".", ". ")
+        n = line.replace(",", ", ")
+        n = line.replace("!", "! ")
+        n = line.replace("?", "? ")
+        n = line.replace("  ", " ")
 
 
-print(first)  # выводит список первых слов
+    d = {}
+    e = []
+    s = list(map(str, n.split()))
+    for j in range(1, len(s)):
 
-print(last)  # выводит список последних слов
+        if not s[j-1] in d:
+            d[s[j-1]] = []
+        d[s[j - 1]].append(s[j])
+        if  len(d[s[j - 1]]) > 1:
+            if s[j-1] not in e:
+                e.append(s[j - 1])
 
-print(e)  # выводит список слов не с одним значением ключа
+    for i in range(len(s)):
+        if s[i][0] == s[i][0].upper():
+            if s[i][-1] not in "!?.":
+                first.append(s[i])
+        if s[i][-1] in ".!?":
+            last.append(s[i])
 
-print(d)  # выводит словарь
 
-for i in range(pov):
-    nachalo = random.choice(first)
-    indnach = list(d.keys()).index(nachalo)
-    for j in range(pov):
+    print(first)  # выводит список первых слов
+
+    print(last)  # выводит список последних слов
+
+    print(e)  # выводит список слов не с одним значением ключа
+
+    print(d)  # выводит словарь
+    print(len(d))
+    for i in range(pov):
+        nachalo = random.choice(first)
+        indnach = list(d.keys()).index(nachalo)
         slovo = random.choice(d.get(nachalo))
-        for l in range(10):
-            slovo1 = random.choice(d.get(slovo))
-print(nachalo, end=' ')
-print(slovo, slovo1)
+        coun = random.choice(range(5, 21))
+        print(nachalo, end = " ")
+        for j in range(coun):
+            slovo = random.choice(d.get(slovo))
+            if slovo[-1] != ".":
+                print(slovo, end = " ")
+            if j == (coun - 1):
+                while slovo[-1] != ".":
+                    slovo = random.choice(d.get(slovo))
+                print(slovo, end = " ")
