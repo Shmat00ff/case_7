719a2e5a8039d66d9e51b64fbee17c63b80e03d9
AnyaBayanova <bayanova-anna@mail.ru>
2018-12-24 20:39:51 +0700

    nonetype

--- case.py
+++ case.py
@@ -9,77 +9,78 @@ first = []
 last = []
 
 text = input('Введите имя файла: ')
-with open(text) as n:
-
+with open(text) as f_in:
+    f = ""
+    for li in f_in:
+        f = f + " " + li
     pov = int(input('Количество генерируемых предложений: '))
-    for line in n:
-        n = line.replace("\n", " ")
-        n = line.replace("&", "")
-        n = line.replace("^", "")
-        n = line.replace("%", "")
-        n = line.replace("$", "")
-        n = line.replace("#", "")
-        n = line.replace("*", "")
-        n = line.replace("-", "")
-        n = line.replace("=", "")
-        n = line.replace("+", "")
-        n = line.replace("@", "")
-        n = line.replace(")", "")
-        n = line.replace("(", "")
-        n = line.replace("/", "")
-        n = line.replace(":", "")
-        n = line.replace(";", "")
+    for line in f_in:
+        line = line.replace("\n", " ")
+        line = line.replace("&", "")
+        line = line.replace("^", "")
+        line = line.replace("%", "")
+        line = line.replace("$", "")
+        line = line.replace("#", "")
+        line = line.replace("*", "")
+        line = line.replace("-", "")
+        line = line.replace("=", "")
+        line = line.replace("+", "")
+        line = line.replace("@", "")
+        line = line.replace(")", "")
+        line = line.replace("(", "")
+        line = line.replace("/", "")
+        line = line.replace(":", "")
+        line = line.replace(";", "")
 
-        n = line.replace(" .", ".")
-        n = line.replace(" ,", ",")
-        n = line.replace(" !", "!")
-        n = line.replace(" ?", "?")
-        n = line.replace(".", ". ")
-        n = line.replace(",", ", ")
-        n = line.replace("!", "! ")
-        n = line.replace("?", "? ")
-        n = line.replace("  ", " ")
-
-
-    d = {}
-    e = []
-    s = list(map(str, n.split()))
-    for j in range(1, len(s)):
-
-        if not s[j-1] in d:
-            d[s[j-1]] = []
-        d[s[j - 1]].append(s[j])
-        if  len(d[s[j - 1]]) > 1:
-            if s[j-1] not in e:
-                e.append(s[j - 1])
-
-    for i in range(len(s)):
-        if s[i][0] == s[i][0].upper():
-            if s[i][-1] not in "!?.":
-                first.append(s[i])
-        if s[i][-1] in ".!?":
-            last.append(s[i])
+        line = line.replace(" .", ".")
+        line = line.replace(" ,", ",")
+        line = line.replace(" !", "!")
+        line = line.replace(" ?", "?")
+        line = line.replace(".", ". ")
+        line = line.replace(",", ", ")
+        line = line.replace("!", "! ")
+        line = line.replace("?", "? ")
+        line = line.replace("  ", " ")
 
 
-    print(first)  # выводит список первых слов
+d = {}
+e = []
+f_in = str(f_in)
+s = list(f_in.split())
+for j in range(1, len(s)):
+    if not s[j-1] in d:
+        d[s[j-1]] = []
+    d[s[j - 1]].append(s[j])
+    if  len(d[s[j - 1]]) > 1:
+        if s[j-1] not in e:
+            e.append(s[j - 1])
 
-    print(last)  # выводит список последних слов
+for i in range(len(s)):
+    if s[i][0] == s[i][0].upper():
+        if s[i][-1] not in "!?.":
+            first.append(s[i])
+    if s[i][-1] in ".!?":
+        last.append(s[i])
 
-    print(e)  # выводит список слов не с одним значением ключа
 
-    print(d)  # выводит словарь
-    print(len(d))
-    for i in range(pov):
-        nachalo = random.choice(first)
-        indnach = list(d.keys()).index(nachalo)
-        slovo = random.choice(d.get(nachalo))
-        coun = random.choice(range(5, 21))
-        print(nachalo, end = " ")
-        for j in range(coun):
-            slovo = random.choice(d.get(slovo))
-            if slovo[-1] != ".":
-                print(slovo, end = " ")
-            if j == (coun - 1):
-                while slovo[-1] != ".":
-                    slovo = random.choice(d.get(slovo))
-                print(slovo, end = " ")
+print(first)  # выводит список первых слов
+
+print(last)  # выводит список последних слов
+
+print(e)  # выводит список слов не с одним значением ключа
+
+print(d)  # выводит словарь
+for i in range(pov):
+    nachalo = random.choice(first)
+    indnach = list(d.keys()).index(nachalo)
+    slovo = random.choice(d.get(nachalo))
+    coun = random.choice(range(5, 21))
+    print(nachalo, end = " ")
+    for j in range(coun):
+        slovo = random.choice(d.get(slovo))
+        if slovo[-1] != ".":
+            print(slovo, end = " ")
+        if j == (coun - 1):
+            while slovo[-1] != ".":
+                slovo = random.choice(d.get(slovo))
+            print(slovo, end = " ")
