861bca9661b2b86190799f05760b1d771118c246
AnyaBayanova <bayanova-anna@mail.ru>
2018-12-24 21:44:17 +0700

    nonetype

--- case.py
+++ case.py
@@ -9,59 +9,56 @@ first = []
 last = []
 
 text = input('Введите имя файла: ')
-with open(text) as f_in:
-    f = ""
-    for li in f_in:
-        f = f + " " + li
-    pov = int(input('Количество генерируемых предложений: '))
-    for line in f_in:
-        line = line.replace("\n", " ")
-        line = line.replace("&", "")
-        line = line.replace("^", "")
-        line = line.replace("%", "")
-        line = line.replace("$", "")
-        line = line.replace("#", "")
-        line = line.replace("*", "")
-        line = line.replace("-", "")
-        line = line.replace("=", "")
-        line = line.replace("+", "")
-        line = line.replace("@", "")
-        line = line.replace(")", "")
-        line = line.replace("(", "")
-        line = line.replace("/", "")
-        line = line.replace(":", "")
-        line = line.replace(";", "")
+f_in = open(text, 'r')
+pov = int(input('Количество генерируемых предложений: '))
+for line in f_in:
+    line = line.replace("&", "")
+    line = line.replace("^", "")
+    line = line.replace("%", "")
+    line = line.replace("$", "")
+    line = line.replace("#", "")
+    line = line.replace("*", "")
+    line = line.replace("-", "")
+    line = line.replace("=", "")
+    line = line.replace("+", "")
+    line = line.replace("@", "")
+    line = line.replace(")", "")
+    line = line.replace("(", "")
+    line = line.replace("/", "")
+    line = line.replace(":", "")
+    line = line.replace(";", "")
 
-        line = line.replace(" .", ".")
-        line = line.replace(" ,", ",")
-        line = line.replace(" !", "!")
-        line = line.replace(" ?", "?")
-        line = line.replace(".", ". ")
-        line = line.replace(",", ", ")
-        line = line.replace("!", "! ")
-        line = line.replace("?", "? ")
-        line = line.replace("  ", " ")
+    line = line.replace(" .", ".")
+    line = line.replace(" ,", ",")
+    line = line.replace(" !", "!")
+    line = line.replace(" ?", "?")
+    line = line.replace(".", ". ")
+    line = line.replace(",", ", ")
+    line = line.replace("!", "! ")
+    line = line.replace("?", "? ")
+    line = line.replace("  ", " ")
+    print(f_in)
 
 
-d = {}
-e = []
-f_in = str(f_in)
-s = list(f_in.split())
-for j in range(1, len(s)):
-    if not s[j-1] in d:
-        d[s[j-1]] = []
-    d[s[j - 1]].append(s[j])
-    if  len(d[s[j - 1]]) > 1:
-        if s[j-1] not in e:
-            e.append(s[j - 1])
+    d = {}
+    e = []
+    s = list(f_in.split())  # за место line было f_in
+    for j in range(1, len(s)):
+        if not s[j-1] in d:
+            d[s[j-1]] = []
+        d[s[j - 1]].append(s[j])
+        if  len(d[s[j - 1]]) > 1:
+            if s[j-1] not in e:
+                e.append(s[j - 1])
 
-for i in range(len(s)):
-    if s[i][0] == s[i][0].upper():
-        if s[i][-1] not in "!?.":
-            first.append(s[i])
-    if s[i][-1] in ".!?":
-        last.append(s[i])
+    for i in range(len(s)):
+        if s[i][0] == s[i][0].upper():
+            if s[i][-1] not in "!?.":
+                first.append(s[i])
+        if s[i][-1] in ".!?":
+            last.append(s[i])
 
+    print(d)  # выводит словарь
 
 print(first)  # выводит список первых слов
 
@@ -69,10 +66,9 @@ print(last)  # выводит список последних слов
 
 print(e)  # выводит список слов не с одним значением ключа
 
-print(d)  # выводит словарь
+
 for i in range(pov):
     nachalo = random.choice(first)
-    indnach = list(d.keys()).index(nachalo)
     slovo = random.choice(d.get(nachalo))
     coun = random.choice(range(5, 21))
     print(nachalo, end = " ")
